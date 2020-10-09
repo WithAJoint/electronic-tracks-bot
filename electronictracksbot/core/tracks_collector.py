b85393aa1d827b09adda401a74998270bface9d3
WithAJoint <WithAJoint@protonmail.com>
2020-10-09 10:54:25 +0200

    fix unremovable trailing space bug

--- electronictracksbot/core/tracks_collector.py
+++ electronictracksbot/core/tracks_collector.py
@@ -1,6 +1,11 @@
 from pytube import YouTube
 
 
+def strip_nonprintable(text):
+    printable_text = text.encode('ascii', 'ignore')
+    return printable_text.strip().decode('utf-8')
+
+
 class TracksCollector:
 
     def __init__(self, base_download_path):
@@ -16,7 +21,7 @@ class TracksCollector:
     def _extract_author_title(self):
         author_title = self._track.title.split(sep='-', maxsplit=1)
         if len(author_title) > 0:
-            self._author, self._title = map(lambda x: x.strip(), author_title)
+            self._author, self._title = map(strip_nonprintable, author_title)
         else:
             raise RuntimeError('Error occurred while retrieving track author and title')
 
