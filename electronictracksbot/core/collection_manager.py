73bccf4bd332a22e3576728b071d9f2db935e333
WithAJoint <WithAJoint@protonmail.com>
2020-10-13 09:38:37 +0200

    implement file sending

--- electronictracksbot/core/collection_manager.py
+++ electronictracksbot/core/collection_manager.py
@@ -7,16 +7,15 @@ class CollectionTrack(GenericTrack):
     def __init__(self, author, title, filepath, is_new=False):
         super().__init__(author, title, filepath)
         self._is_new = is_new
-        self._new_author = self._new_title = None
 
     def is_new(self):
         return self._is_new
 
-    def edit_author(self, author):
-        self._new_author = author
-
-    def edit_title(self, title):
-        self._new_title = title
+    def edit(self, *, author=None, title=None):
+        if author:
+            self._author = author
+        elif title:
+            self._title = title
 
     @staticmethod
     def create_from_generic(track: GenericTrack, is_new=True):
@@ -29,14 +28,14 @@ class CollectionManager:
         self._tracks_collector = TrackCollector(download_path)
         self._database_manager = DatabaseManager(db_path)
 
-    def collect_from_youtube(self, track_link) -> CollectionTrack:
+    def preview_details(self, track_link) -> CollectionTrack:
         track = self._tracks_collector.acquire_metadata(track_link)
-        if self._database_manager.exists_track(track.get_author(), track.get_title()):
+        return CollectionTrack.create_from_generic(track)
+
+    def collect_if_new(self, track: CollectionTrack, duplication=False) -> CollectionTrack:
+        if self._database_manager.exists_track(track.get_author(), track.get_title()) and not duplication:
             filepath = self._database_manager.retrieve_track_filepath(track.get_author(), track.get_title())
             return CollectionTrack(track.get_author(), track.get_title(), filepath)
         self._tracks_collector.collect_acquired()
         self._database_manager.insert_track(track.get_author(), track.get_title(), track.get_filepath())
-        return CollectionTrack.create_from_generic(track)
-
-    def update_track_details(self, track: CollectionTrack):
-        pass
+        return track
--- electronictracksbot/electronic_tracks_bot.py
+++ electronictracksbot/electronic_tracks_bot.py
@@ -40,10 +40,11 @@ class DialogScheme:
 
 
 class ElectronicTracksBot:
-    _MAIN_MENU, _EDIT_PROPERTY, _SET_NEW_VALUE = range(3)
+    _MAIN_MENU, _EDIT_PROPERTY, _SET_NEW_VALUE, _CONFIRM_DUPLICATION = range(4)
 
     _MAIN_DIALOG = DialogScheme('- Author -\n{}\n- Title -\n{}\n',
-                                InlineKeyboardMarkup([[InlineKeyboardButton('Edit', callback_data='EDIT'),
+                                InlineKeyboardMarkup([[InlineKeyboardButton('Exit', callback_data='EXIT'),
+                                                       InlineKeyboardButton('Edit', callback_data='EDIT'),
                                                        InlineKeyboardButton('Send', callback_data='SEND')]]))
 
     _EDIT_DIALOG = DialogScheme('- Author -\n{}\n- Title -\n{}\n',
@@ -53,13 +54,17 @@ class ElectronicTracksBot:
 
     _SET_DIALOG = DialogScheme('Send new {}\n')
 
-    _DUPLICATE_WARNING_DIALOG = DialogScheme.create_fixed('This track is already in the collection')
+    _DUPLICATE_WARNING_DIALOG = DialogScheme \
+        .create_fixed('This track is already in the collection.\nDo you want to send it anyways?',
+                      InlineKeyboardMarkup([[InlineKeyboardButton('Back', callback_data='BACK'),
+                                             InlineKeyboardButton('Send', callback_data='SEND:duplicate')]]))
 
     _DOWNLOAD_ERROR_DIALOG = DialogScheme.create_fixed('Error while downloading the track. Check logs')
 
-    def __init__(self, api_token, collection_manager):
+    def __init__(self, api_token, collection_manager, destination_channel):
         self._updater = Updater(api_token, use_context=True)
         self._collection_manager = collection_manager
+        self._destination_channel = destination_channel
         self._init_handlers(self._updater.dispatcher)
 
     def _init_handlers(self, dispatcher: Dispatcher):
@@ -67,10 +72,13 @@ class ElectronicTracksBot:
             entry_points=[MessageHandler(Filters.entity('url'), self._collect_track)],
             states={
                 self._MAIN_MENU: [CallbackQueryHandler(self._enter_edit_mode, pattern='^EDIT?'),
-                                  CallbackQueryHandler(self._send_track, pattern='^SEND?')],
+                                  CallbackQueryHandler(self._check_send_track, pattern='^SEND?'),
+                                  CallbackQueryHandler(lambda u, c: ConversationHandler.END, pattern='^EXIT?')],
                 self._EDIT_PROPERTY: [CallbackQueryHandler(self._return_to_main_menu, pattern='^BACK?'),
                                       CallbackQueryHandler(self._select_property_to_edit, pattern='^AUTHOR?|^TITLE?')],
-                self._SET_NEW_VALUE: [MessageHandler(Filters.all, self._set_new_value)]
+                self._SET_NEW_VALUE: [MessageHandler(Filters.all, self._set_new_value)],
+                self._CONFIRM_DUPLICATION: [CallbackQueryHandler(self._return_to_main_menu, pattern='^BACK?'),
+                                            CallbackQueryHandler(self._force_send, pattern='^SEND:duplicate?')]
             },
             fallbacks=[]
         ))
@@ -78,44 +86,57 @@ class ElectronicTracksBot:
     def _collect_track(self, update: Update, context: CallbackContext):
         track_link = update.message.text
         try:
-            track = self._collection_manager.collect_from_youtube(track_link)
+            track = self._collection_manager.preview_details(track_link)
         except KeyError:
             self._reply(update.message, **self._DOWNLOAD_ERROR_DIALOG.parameterize())
-            return None
-        if not track.is_new():
-            self._reply(update.message, **self._DUPLICATE_WARNING_DIALOG.parameterize())
-        _insert_context_property(context, {'AUTHOR': track.get_author(), 'TITLE': track.get_title()})
+            return ConversationHandler.END
+        context.user_data['TRACK'] = track
         dialog = self._MAIN_DIALOG.finalize(track.get_author(), track.get_title())
         self._reply(update.message, **dialog.parameterize())
         return self._MAIN_MENU
 
     def _enter_edit_mode(self, update: Update, context: CallbackContext):
-        dialog = self._EDIT_DIALOG.finalize(context.user_data['AUTHOR'], context.user_data['TITLE'])
+        track = context.user_data['TRACK']
+        dialog = self._EDIT_DIALOG.finalize(track.get_author(), track.get_title())
         self._reply(update.callback_query, **dialog.parameterize())
         return self._EDIT_PROPERTY
 
     def _return_to_main_menu(self, update: Update, context: CallbackContext):
-        dialog = self._MAIN_DIALOG.finalize(context.user_data['AUTHOR'], context.user_data['TITLE'])
+        track = context.user_data['TRACK']
+        dialog = self._MAIN_DIALOG.finalize(track.get_author(), track.get_title())
         self._reply(update.callback_query, **dialog.parameterize())
         return self._MAIN_MENU
 
     def _select_property_to_edit(self, update: Update, context: CallbackContext):
         query = update.callback_query
-        context.user_data['EDIT'] = query.data
+        context.user_data['EDIT'] = query.data.lower()
         dialog = self._SET_DIALOG.finalize(query.data)
         self._reply(query, **dialog.parameterize())
         return self._SET_NEW_VALUE
 
     def _set_new_value(self, update: Update, context: CallbackContext):
-        new_value = update.message.text
-        property_to_edit = context.user_data['EDIT']
-        context.user_data[property_to_edit] = new_value
-        dialog = self._MAIN_DIALOG.finalize(context.user_data['AUTHOR'], context.user_data['TITLE'])
+        track = context.user_data['TRACK']
+        track.edit(**{context.user_data['EDIT']: update.message.text})
+        dialog = self._MAIN_DIALOG.finalize(track.get_author(), track.get_title())
         self._reply(update.message, **dialog.parameterize())
         return self._MAIN_MENU
 
-    def _send_track(self, update: Update, context: CallbackContext):
-        pass
+    def _check_send_track(self, update: Update, context: CallbackContext):
+        context.user_data['TRACK'] = track = self._collection_manager.collect_if_new(context.user_data['TRACK'])
+        if not track.is_new():
+            self._reply(update.callback_query, **self._DUPLICATE_WARNING_DIALOG.parameterize())
+            return self._CONFIRM_DUPLICATION
+        self._send_file(context, track)
+        return ConversationHandler.END
+
+    def _force_send(self, update: Update, context: CallbackContext):
+        track = self._collection_manager.collect_if_new(context.user_data['TRACK'], duplication=True)
+        self._send_file(context, track)
+        return ConversationHandler.END
+
+    def _send_file(self, context, track):
+        context.bot.send_audio(self._destination_channel, audio=open(track.get_filepath(), 'rb'),
+                               performer=track.get_author(), title=track.get_title())
 
     @singledispatchmethod
     def _reply(self, obj, **dialog):
@@ -139,9 +160,10 @@ if __name__ == '__main__':
     config_reader = ConfigReader()
     api_token = config_reader.get('TELEGRAM', 'api-token')
     db_path = config_reader.get("DATABASE", 'path')
-    download_path = config_reader.get("DOWNLOAD", 'folder')
+    download_path = config_reader.get('DOWNLOAD', 'folder')
+    destination_channel = config_reader.get('CHANNEL', 'id')
 
     collection_manager = CollectionManager(db_path, download_path)
 
-    electronic_tracks_bot = ElectronicTracksBot(api_token, collection_manager)
+    electronic_tracks_bot = ElectronicTracksBot(api_token, collection_manager, destination_channel)
     electronic_tracks_bot.start_accepting_requests()
